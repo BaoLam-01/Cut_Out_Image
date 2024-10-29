47988eaa20c83cc737f8b66bf0b635fbfb99f670
DESKTOP-IVN453T\Admin <lam.dinhvan@tapbi.com>
2024-10-29 16:25:53 +0700

    draw to cut success!

--- app/src/main/java/com/lampro/cutoutimage/CutOutView.kt
+++ app/src/main/java/com/lampro/cutoutimage/CutOutView.kt
@@ -2,15 +2,25 @@ package com.lampro.cutoutimage
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Color
+import android.graphics.CornerPathEffect
+import android.graphics.Matrix
 import android.graphics.Paint
 import android.graphics.Path
+import android.graphics.PorterDuff
+import android.graphics.PorterDuffXfermode
+import android.graphics.Rect
+import android.graphics.RectF
 import android.util.AttributeSet
 import android.util.Log
 import android.view.DragEvent
 import android.view.MotionEvent
 import android.view.View
+import android.widget.Toast
+import kotlin.math.pow
+import kotlin.math.sqrt
 
 class CutOutView : View {
 
@@ -19,9 +29,21 @@ class CutOutView : View {
 
     private var currentX = 0f
     private var currentY = 0f
+    private var startX = 0f
+    private var startY = 0f
 
-    constructor(context: Context) : super(context)
+    private var isReady = false
+
+    constructor(context: Context) : super(context, null)
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+
+    private lateinit var listener : (isReady: Boolean) -> Unit
+
+    private var viewCanvasWidth: Int = 0
+    private var viewCanvasHeight : Int = 0
+
+    private var targetOriginalBitmap: Bitmap? = null
+    private var actualVisibleBitmap: Bitmap? = null
 
     private val TAG = "CutOutView"
 
@@ -38,25 +60,72 @@ class CutOutView : View {
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
-        // Account for padding.
-        var xpad = (paddingLeft + paddingRight).toFloat()
-        val ypad = (paddingTop + paddingBottom).toFloat()
+    }
 
-        // Account for the label.
-//        if (showText) xpad += textWidth.toFloat()
-//        val ww = w.toFloat() - xpad
-//        val hh = h.toFloat() - ypad
-//
-        // Figure out how big you can make the pie.
-//        val diameter = Math.min(ww, hh)
+    fun setImageBitmap(imageBitmap: Bitmap) {
+        targetOriginalBitmap = imageBitmap
+        actualVisibleBitmap = null
+        invalidate()
     }
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-        canvas.drawPath(path,paint)
+        viewCanvasWidth = width
+        viewCanvasHeight = height
+
+        if (targetOriginalBitmap != null) {
+            // If target's original bitmap is bigger than view size, adjust size for fit
+            if (actualVisibleBitmap == null) actualVisibleBitmap = scaleBitmapAndKeepRation(
+                targetOriginalBitmap!!,
+                    height,
+                    width
+                )
+            canvas.drawBitmap(
+                actualVisibleBitmap!!,
+                (viewCanvasWidth / 2 - actualVisibleBitmap!!.width / 2).toFloat(),
+                (viewCanvasHeight / 2 - actualVisibleBitmap!!.height / 2).toFloat(),
+                null
+            )
+            canvas.drawPath(path,paint)
+
+        } else {
+            canvas.drawColor(Color.WHITE)
+
+            val textPaint = Paint()
+            textPaint.color = Color.BLACK
+            textPaint.textSize = 20f
+            textPaint.isAntiAlias = true
+
+            canvas.drawText(
+                "Please set image bitmap for process",
+                (width/3).toFloat(),
+                (height/2).toFloat(),
+                textPaint
+            )
+            isReady = false
+
+        }
+
     }
 
+    fun scaleBitmapAndKeepRation(
+        TargetBmp: Bitmap,
+        reqHeightInPixels: Int,
+        reqWidthInPixels: Int
+    ): Bitmap {
+        val m = Matrix()
+        m.setRectToRect(
+            RectF(0f, 0f, TargetBmp.width.toFloat(), TargetBmp.height.toFloat()),
+            RectF(0f, 0f, reqWidthInPixels.toFloat(), reqHeightInPixels.toFloat()),
+            Matrix.ScaleToFit.CENTER
+        )
+        return Bitmap.createBitmap(TargetBmp, 0, 0, TargetBmp.width, TargetBmp.height, m, true)
+    }
+    @SuppressLint("ClickableViewAccessibility")
     override fun onTouchEvent(event: MotionEvent?): Boolean {
+        if (targetOriginalBitmap == null) {
+            return false
+        }
 
 
         event?.let {
@@ -66,28 +135,60 @@ class CutOutView : View {
 
             when (event.action) {
                 MotionEvent.ACTION_DOWN -> {
-                    // Khi bắt đầu chạm, di chuyển path đến vị trí chạm
+                    path.reset()
                     path.moveTo(x, y)
                     currentX = x
                     currentY = y
+
+                    startX = x
+                    startY = y
+
+                    isReady = false
+                    listener(isReady)
                 }
                 MotionEvent.ACTION_MOVE -> {
-                    // Khi di chuyển, thêm các điểm vào path
+
                     path.quadTo(currentX, currentY, (x + currentX) / 2, (y + currentY) / 2)
                     currentX = x
                     currentY = y
+
                 }
                 MotionEvent.ACTION_UP -> {
-                    // Khi kết thúc chạm, vẽ đường đến điểm cuối
-                    path.lineTo(currentX, currentY)
+                    path.quadTo(currentX, currentY, (x + currentX) / 2, (y + currentY) / 2)
+                    
+                    val distance = calculateDistance(startX,startY,currentX,currentY)
+
+                    if (distance < 100) {
+                        path.quadTo(x,y, startX , startY)
+
+                        isReady = true
+                        listener(isReady)
+                    } else
+                    {
+                        Toast.makeText(
+                            this.context,
+                            context.getString(R.string.toast_text_cut_out),
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        path.reset()
+                        isReady = false
+                        listener(isReady)
+                        invalidate()
+                    }
                 }
             }
 
-            invalidate() // Gọi vẽ lại màn hình
+            invalidate()
             return true
         }
 
         return super.onTouchEvent(event)
+    }
+
+    private fun calculateDistance(startX: Float, startY: Float, endX: Float, endY: Float) : Double {
+        val offsetX = endX - startX
+        val offsetY = endY - startY
+        return sqrt(offsetX.toDouble().pow(2.0) + offsetY.toDouble().pow(2.0))
     }
 
     override fun onHoverEvent(event: MotionEvent?): Boolean {
@@ -102,6 +203,80 @@ class CutOutView : View {
             return true
         }
         return super.onDragEvent(event)
+    }
+
+    fun cropImageWithPath(): Bitmap {
+
+        val resultImage =
+            Bitmap.createBitmap(viewCanvasWidth, viewCanvasHeight, actualVisibleBitmap!!.config)
+
+        val resultCanvas = Canvas(resultImage)
+        val resultPaint = Paint()
+
+
+        // struct paint for naturally
+        resultPaint.isAntiAlias = true
+        resultPaint.isDither = true // set the dither to true
+        resultPaint.strokeJoin = Paint.Join.ROUND // set the join to round you want
+        resultPaint.strokeCap = Paint.Cap.ROUND // set the paint cap to round too
+        resultPaint.setPathEffect(CornerPathEffect(10f))
+        resultPaint.isAntiAlias = true // set anti alias so it smooths
+
+
+        // struct paint for path-crop
+        resultCanvas.drawPath(path, resultPaint)
+        resultPaint.setXfermode(PorterDuffXfermode(PorterDuff.Mode.SRC_IN))
+
+        val dst = Rect(
+            viewCanvasWidth / 2 - actualVisibleBitmap!!.width / 2,
+            viewCanvasHeight / 2 - actualVisibleBitmap!!.height / 2,
+            viewCanvasWidth / 2 + actualVisibleBitmap!!.width / 2,
+            viewCanvasHeight / 2 + actualVisibleBitmap!!.height / 2
+        )
+
+        resultCanvas.drawBitmap(actualVisibleBitmap!!, null, dst, resultPaint)
+
+        return BitmapUtils.cropBitmapToBoundingBox(resultImage, Color.TRANSPARENT)
+    }
+
+    fun cropImageWithShape(): Bitmap{
+        val resultImage =
+            Bitmap.createBitmap(viewCanvasWidth, viewCanvasHeight, actualVisibleBitmap!!.config)
+
+        val resultCanvas = Canvas(resultImage)
+        val resultPaint = Paint()
+
+
+        // struct paint for naturally
+        resultPaint.isAntiAlias = true
+        resultPaint.isDither = true // set the dither to true
+        resultPaint.strokeJoin = Paint.Join.ROUND // set the join to round you want
+        resultPaint.strokeCap = Paint.Cap.ROUND // set the paint cap to round too
+        resultPaint.setPathEffect(CornerPathEffect(10f))
+        resultPaint.isAntiAlias = true // set anti alias so it smooths
+
+
+        // struct paint for path-crop
+        resultCanvas.drawPath(path, resultPaint)
+        resultPaint.setXfermode(PorterDuffXfermode(PorterDuff.Mode.SRC_IN))
+
+        val dst = Rect(
+            viewCanvasWidth / 2 - actualVisibleBitmap!!.width / 2,
+            viewCanvasHeight / 2 - actualVisibleBitmap!!.height / 2,
+            viewCanvasWidth / 2 + actualVisibleBitmap!!.width / 2,
+            viewCanvasHeight / 2 + actualVisibleBitmap!!.height / 2
+        )
+        resultCanvas.drawBitmap(actualVisibleBitmap!!, null, dst, resultPaint)
+
+        return resultImage
+    }
+
+    fun getIsReady() : Boolean {
+        return isReady
+    }
+
+    fun setOnCutOutListener(callback: (isReady: Boolean) -> Unit) {
+            listener = callback
     }
 
 }
--- app/src/main/java/com/lampro/cutoutimage/MainActivity.kt
+++ app/src/main/java/com/lampro/cutoutimage/MainActivity.kt
@@ -1,12 +1,32 @@
 package com.lampro.cutoutimage
 
+import android.content.ContentValues.TAG
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.drawable.toBitmap
+import androidx.core.net.toUri
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.lampro.cutoutimage.databinding.ActivityMainBinding
+import java.io.File
+import java.io.FileOutputStream
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +36,43 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val cutOutView = findViewById<CutOutView>(R.id.cutOutView)
+        val btnNext = findViewById<TextView>(R.id.tvNext)
+        val imgMeo = findViewById<ImageView>(R.id.imgMeo)
+
+        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.meo)
+        cutOutView.setImageBitmap(bitmap)
+
+        cutOutView.setOnCutOutListener { isReady ->
+                if (isReady) {
+                    btnNext.visibility = View.VISIBLE
+                } else {
+                    btnNext.visibility = View.INVISIBLE
+                }
+        }
+
+
+
+        btnNext.setOnClickListener{
+
+            val outBitmap = cutOutView.cropImageWithPath()
+
+            val uri  = saveBitmapToFile(outBitmap,this)
+
+            val intent = Intent(this, OutputActivity::class.java)
+
+            intent.putExtra("bitmapUri", uri.toString())
+            startActivity(intent)
+        }
     }
+
+    private fun saveBitmapToFile(bitmap: Bitmap, context: Context): Uri {
+        val file = File(context.cacheDir, "cutOutImage.png")
+        FileOutputStream(file).use { out ->
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, out)
+        }
+        return file.toUri()
+    }
+
 }
