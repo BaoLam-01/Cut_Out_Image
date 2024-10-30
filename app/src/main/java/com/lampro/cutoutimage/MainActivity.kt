e93997cbc63b374e2131be7b4b9f203a8cf398a7
DESKTOP-IVN453T\Admin <lam.dinhvan@tapbi.com>
2024-10-30 18:09:13 +0700

    fix

--- app/src/main/java/com/lampro/cutoutimage/MainActivity.kt
+++ app/src/main/java/com/lampro/cutoutimage/MainActivity.kt
@@ -1,25 +1,18 @@
 package com.lampro.cutoutimage
 
-import android.content.ContentValues.TAG
 import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import android.view.View
-import android.widget.Button
-import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.graphics.drawable.toBitmap
 import androidx.core.net.toUri
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.lampro.cutoutimage.databinding.ActivityMainBinding
 import java.io.File
 import java.io.FileOutputStream
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,4 +21,3 @@ dependencyResolutionManagement {
 
 rootProject.name = "Cut Out Image"
 include(":app")
- 
